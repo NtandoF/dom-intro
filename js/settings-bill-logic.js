a95fe195d2eca87d7cd0ebe013de1527b8d73816
Ntando <ntandofitshaneversace@gmail.com>
2018-05-21 11:52:45 +0200

    final changes

--- js/settings-bill-logic.js
+++ js/settings-bill-logic.js
@@ -1,97 +1,81 @@
-var SettingsbillFactory = function(){
+var SettingFactory = function() {
 
-   var callSettings =0;
-    var  smsSettings =0;
-     var totall =callSettings + smsSettings;
+    var callSettings = 0;
+    var smsSettings = 0;
+    var totalS = 0;
 
-    var callCost =0;
-      var smsCost =0;
-       var warning =0;
-        var critical =0;
+    var callCost = 0;
+    var smsCost = 0;
+    var warning = 0;
+    var critical = 0;
 
-// create a variables that will keep track of all three totals.
-// console.log(TotalSettings);
+    var SettingsBillLogic = function(billType) {
+
+      if (billType === "sms") {
+        smsSettings += smsCost;
+        totalS += smsCost;
 
 
-function settingsBillTotal(billItemTypeWithSettings){
-  // update the correct total
-if (GetTotallsett() < critical){
+      }
+      if (billType === "call") {
+        callSettings += callCost;
+        totalS += callCost;
 
-    if (billItemTypeWithSettings === "call"){
-        callSettings +=  callCost;
-        totall += callCost;
-        }
 
-   if (billItemTypeWithSettings === "sms"){
-        smsSettings +=  smsCost;
-        totall += smsCost;
+      }
+    }
+      //getters
+      var getCallCost = function() {
+        return callSettings;
+      }
+      var getSmsCost = function() {
+        return smsSettings;
+      }
+      var getWarning = function() {
+        return warning;
+      }
+      var getCritical = function() {
+        return critical;
+      }
+      var getTotals = function() {
+        return smsSettings + callSettings;
+      }
+        //setters
+
+      var setCallCost = function(num) {
+        callCost = parseFloat(num);
+      }
+      var setSmsCost = function(num) {
+        smsCost = parseFloat(num);
+      }
+      var setWarning = function(num) {
+        warning = parseFloat(num);
+      }
+      var setCritical = function(num) {
+        critical = parseFloat(num);
       }
 
-   }
- }
+      var ReachedWarning = function() {
+       return getTotals() >= getWarning();
+      }
 
+      var ReachedCritical = function() {
+      return  getTotals() >=getCritical();
+      }
 
+      return {
+        SettingsBillLogic,
+        getCallCost,
+        getSmsCost,
+        getWarning,
+        getCritical,
+        getTotals,
+        setCallCost,
+        setSmsCost,
+        setWarning,
+        setCritical,
+        ReachedWarning,
+        ReachedCritical
 
-   //setters
-   function SetCallcost(num){
-     callCost = parseFloat(num);
-     // console.log(callCost);
-
-   }
-
-   function SetSmscost(num) {
-    smsCost = parseFloat(num);
-   }
-
-   function SetWarning(num) {
-    warning = parseFloat(num);
-   }
-   function SetCritical(num){
-    critical = parseFloat(num);
-   }
-
-   //getters
-   function GetCritical(){
-   return critical;
-   }
-
-   function GetWarning() {
-     return warning;
-   }
-
-   function GetCallSett() {
-      return callSettings;
-   }
-
-   function GetSmsSett() {
-    return smsSettings;
-   }
-
-   function GetTotallsett(){
-    return callSettings + smsSettings;
-   }
-
-   function hasReachedCritical(){
-      return GetTotallsett() >= GetCritical()
-   }
-   function hasReachedWarning(){
-      return GetTotallsett() >= GetWarning()
-   }
-
-
-return {
- settingsBillTotal,
- SetCallcost,
- SetSmscost,
- SetWarning,
- SetCritical,
- GetWarning,
- GetCritical,
- GetCallSett,
- GetSmsSett,
- hasReachedCritical,
- hasReachedWarning,
- GetTotallsett
-
-       }
-}
+      }
+    }
--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,74 +1,72 @@
-
-   var billItemTypeWithSettings = document.querySelector(".billItemTypeWithSettings");
+// get a reference to the sms or call radio buttons
+var billItemTypeWithSettings = document.querySelector(".billItemTypeWithSettings");
 
 // get refences to all the settings fields
-  var warningLevelSetting = document.querySelector(".warningLevelSetting");
-    var callCostSettings = document.querySelector(".callCostSetting");
-      var smsCostSetting = document.querySelector(".smsCostSetting");
-        var criticalLevelSetting = document.querySelector(".criticalLevelSetting");
-
-        var callTotalSettings = document.querySelector(".callTotalSettings");
-         var smsTotalSettings = document.querySelector(".smsTotalSettings");
-          var TotalSettings = document.querySelector(".totalSettings");
-
+var warningLevelSetting = document.querySelector(".warningLevelSetting");
+var callCostSettings = document.querySelector(".callCostSetting");
+var smsCostSetting = document.querySelector(".smsCostSetting");
+var criticalLevelSetting = document.querySelector(".criticalLevelSetting");
 
 //get a reference to the add button
-  var settingAddBtn = document.querySelector(".SettingsAddBtn");
+var settingsBillAddBtn = document.querySelector(".SettingsAddBtn");
 
 //get a reference to the 'Update settings' button
-  var updateBtn = document.querySelector(".updateSettings");
-// create a variables that will keep track of all the settings
+var updateSettingsBtn = document.querySelector(".updateSettings");
+// console.log(callsTotalElem.innerHTML);
 
 
 
-var factorys = SettingsbillFactory();
+// create a variables that will keep track of all three totals.
+var callTotalSettings = document.querySelector(".callTotalSettings");
+var smsTotalSettings = document.querySelector(".smsTotalSettings");
+var TotalSettings = document.querySelector(".totalSettings");
+var setFunction = SettingFactory();
+// console.log(TotalSettings);
 
-  function updateSettings(){
-     factorys.SetCallcost(callCostSettings.value);
-     factorys.SetSmscost(smsCostSetting.value);
-    factorys.SetWarning(warningLevelSetting.value);
-    factorys.SetCritical(criticalLevelSetting.value);
+function updateSettings() {
+  setFunction.setCallCost(callCostSettings.value);
+  setFunction.setSmsCost(smsCostSetting.value);
+  setFunction.setWarning(warningLevelSetting.value);
+  setFunction.setCritical(criticalLevelSetting.value);
+}
+
+
+function settingsBillTotal() {
+  //var billItemTypeWithSettings = checkedSettingsBtn.value
+if( setFunction.getTotals()<setFunction.getCritical()){
+  var checkedSettingsBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+  if (checkedSettingsBtn) {
+    setFunction.SettingsBillLogic(checkedSettingsBtn.value);
+
+    callTotalSettings.innerHTML = setFunction.getCallCost().toFixed(2);
+    smsTotalSettings.innerHTML = setFunction.getSmsCost().toFixed(2);
+    TotalSettings.innerHTML = setFunction.getTotals().toFixed(2);
+
+
+    if (setFunction.getTotals() >= setFunction.getWarning()) {
+      // adding the danger class will make the text red
+      TotalSettings.classList.add("warning");
+
+    }
+    if (setFunction.getTotals() >= setFunction.getCritical()) {
+      TotalSettings.classList.add("danger");
+    }
+    if (setFunction.getTotals() < setFunction.getCritical()) {
+      TotalSettings.classList.remove("danger");
+    }
+    if (setFunction.getTotals() < setFunction.getWarning()) {
+      TotalSettings.classList.remove("warning");
+    }
+    // billItemType will be 'call' or 'sms'
   }
-
-
-
-       function settingsBillTotalDom(){
-         // get the value entered in the billType textfield
-         var checkedSettingsBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-         if (checkedSettingsBtn){
-             var billItemTypeWithSettings = checkedSettingsBtn.value
-             // billItemType will be 'call' or 'sms'
-             factorys.settingsBillTotal(billItemTypeWithSettings);
-         }
-        var tot = factorys.GetTotallsett();
-
-        callTotalSettings.innerHTML = factorys.GetCallSett().toFixed(2);
-        TotalSettings.innerHTML = factorys.GetTotallsett().toFixed(2);;
-        smsTotalSettings.innerHTML = factorys.GetSmsSett().toFixed(2);;
-        TotalSettings.innerHTML = factorys.GetTotallsett().toFixed(2);;
-
-          if (tot >= factorys.GetWarning()){
-                  // adding the danger class will make the text red
-                  TotalSettings.classList.add("warning");
-
-              }
-           if (tot >= factorys.GetCritical()){
-                  TotalSettings.classList.add("danger");
-              }
-              if (tot< factorys.GetCritical()){
-                TotalSettings.classList.remove("danger");
-              }
-              if (tot< factorys.GetWarning()){
-                TotalSettings.classList.remove("warning");
-              }
-
-
-
-    //update the totals that is displayed on the screen.
+}
+  // get the value entered in the billType textfield
 
 }
 
 
-settingAddBtn.addEventListener('click', settingsBillTotalDom);
+//update the totals that is displayed on the screen.
 
-updateBtn.addEventListener('click',updateSettings);
+
+settingsBillAddBtn.addEventListener('click', settingsBillTotal);
+updateSettingsBtn.addEventListener('click', updateSettings);
